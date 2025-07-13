1832c5675844cf7a071507f377c300a9ae9cb615
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-07-13 22:00:29 +0330

    Receive user information

--- controllers/authController.go
+++ controllers/authController.go
@@ -4,6 +4,7 @@ import (
 	"JWT-Authentication-go/database"
 	"JWT-Authentication-go/models"
 	"fmt"
+	// "go/token"
 	"strconv"
 	"time"
 
@@ -92,4 +93,34 @@ func Login(c *fiber.Ctx) error{
 
 	c.Cookie(&cookie)
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Login successful"})
+}
+
+
+func User(c *fiber.Ctx) error{
+	fmt.Println("Request to get user")
+	cookie := c.Cookies("jwt")
+
+    token, err := jwt.ParseWithClaims(cookie, &jwt.MapClaims{}, func(token *jwt.Token) (interface{}, error) {
+        return []byte("secretKey"), nil
+    })
+
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error" : "Unauthorized",
+		})
+	}
+
+	claims , ok := token.Claims.(*jwt.MapClaims)
+	if !ok {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error" : "Failed to parse claim",
+		})
+	}
+
+	id , _ := strconv.Atoi((*claims)["sub"].(string))
+	user := models.User{ID : uint(id)}
+
+	database.DB.Where("id=?",id).First(&user)
+
+	return  c.JSON(user)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -10,4 +10,5 @@ func SetupRoutes(app *fiber.App) {
 	app.Get("/", controllers.Hello)
 	app.Post("/api/register", controllers.Register)
 	app.Post("/api/login", controllers.Login)
+	app.Get("/api/user", controllers.User)
 }

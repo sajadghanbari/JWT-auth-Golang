79831bf7fb524fbb73fb4e19f819c31471a8d6bb
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-10-06 17:56:41 +0330

    CORS middleware added

---
+++ api/middlewares/cors.go
@@ -0,0 +1,18 @@
+package middlewares
+
+import (
+	"JWT-Authentication-go/config"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func Cors(cfg *config.Config) fiber.Handler {
+	return cors.New(cors.Config{
+		AllowOrigins:     cfg.Cors.AllowOrigins, 
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS,UPDATE",
+		AllowHeaders:     "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With",
+		MaxAge:           21600, 
+	})
+}
